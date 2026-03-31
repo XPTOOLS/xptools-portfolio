bccc27ca93f3d4baf91ab6537d1206969c89723e
XP TOOLS <freenethubbusiness@gmail.com>
2026-03-31 06:53:44 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['xportfolio-60ep.onrender.com'],
+    host: true,
+    port: 10000,
+  },
+  preview: {
+    allowedHosts: ['xportfolio-60ep.onrender.com'],
+    host: true,
+    port: 10000,
+  },
 })
